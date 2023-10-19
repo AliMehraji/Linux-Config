fb5def50df1cc3087bc0975253b97af065b47c34
Ali Mehraji <a.mehraji75@gmail.com>
2023-10-19 19:28:25 +0330

    Update: robbyrussell.theme.sh for displaying Python venv namein PS1

--- Shell/Bash/oh-my-bash/robbyrussell.theme.sh
+++ Shell/Bash/oh-my-bash/robbyrussell.theme.sh
@@ -10,6 +10,16 @@ function _omb_theme_PROMPT_COMMAND() {
         local arrow_color="${_omb_prompt_bold_brown}"
     fi
 
+    # Check if a Python virtual environment is active
+    if [ -n "$VIRTUAL_ENV" ]; then
+        # ${VIRTUAL_ENV##*/} is a Bash parameter expansion ...
+        # that extracts the base directory name from the value ... 
+        # stored in the VIRTUAL_ENV environment variable.
+        local venv_name="${_omb_prompt_normal_white}(${VIRTUAL_ENV##*/})${_omb_prompt_reset_color}"
+    else
+        local venv_name=""
+    fi
+
     local base_directory="${_omb_prompt_bold_teal}\W${_omb_prompt_reset_color}"
     local GIT_THEME_PROMPT_PREFIX="${_omb_prompt_bold_navy}git:(${_omb_prompt_bold_brown}"
     local SVN_THEME_PROMPT_PREFIX="${_omb_prompt_bold_navy}svn:(${_omb_prompt_bold_brown}"
@@ -19,9 +29,9 @@ function _omb_theme_PROMPT_COMMAND() {
     local SCM_THEME_PROMPT_DIRTY="${_omb_prompt_bold_navy}) ${_omb_prompt_olive}✗${_omb_prompt_reset_color}"
 
     local arrow="${arrow_color}⮞${_omb_prompt_reset_color}"
-    local hostname="${_omb_prompt_white}\u@\h"
+    local hostname="${_omb_prompt_normal_white}\u@\h"
 
-    PS1="${hostname} ${arrow} ${base_directory} "
+    PS1="${venv_name}${hostname} ${arrow} ${base_directory} "
 
     local scm_info=$(scm_prompt_info)
 
